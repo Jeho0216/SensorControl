22d9736e1713119d549af44c5acfc1e155b924ad
Jeho0216 <leejen2003@naver.com>
2016-07-15 15:26:20 +0900

    입력된 0~1024 (10bit)값을 0~255 (8bit)값으로 맵핑

--- SensorControl/SensorControl.ino
+++ SensorControl/SensorControl.ino
@@ -1,6 +1,7 @@
 const int POT = 0;
-
-int val = 0;
+const int MIN_LIGHT = 10;   //가장 어두울 때의 값
+const int MAX_LIGHT = 720;  //가장 밝을 때의 값
+int val = A0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,6 +11,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   val = analogRead(POT);
+  val = map(val, MIN_LIGHT, MAX_LIGHT, 255, 0);   //8비트 값으로 맵핑
+  val = constrain(val, 0, 255);
   Serial.println(val);
   delay(500);
 }
