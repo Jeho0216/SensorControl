4e358064c9a07c0b73e8617b4d3c523125bd9f6b
Jeho0216 <leejen2003@naver.com>
2016-07-15 15:18:50 +0900

    포토레지스터의 변화값 출력(시리얼통신)

--- SensorControl/SensorControl.ino
+++ SensorControl/SensorControl.ino
@@ -1,3 +1,7 @@
+const int POT = 0;
+
+int val = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -5,5 +9,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  val = analogRead(POT);
+  Serial.println(val);
+  delay(500);
 }
