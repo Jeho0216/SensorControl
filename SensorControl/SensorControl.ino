8f95327a121750abb37b9616b47cf75c363acd38
Jeho0216 <leejen2003@naver.com>
2016-07-18 15:01:38 +0900

    LCD Test코드 삽입

--- SensorControl/SensorControl.ino
+++ SensorControl/SensorControl.ino
@@ -1,3 +1,8 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+//RS, E, D4, D5, D6, D7
+
 const int POT = 0;
 const int MIN_LIGHT = 10;   //가장 어두울 때의 값
 const int MAX_LIGHT = 720;  //가장 밝을 때의 값
@@ -6,6 +11,9 @@ int val = A0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.setCursor(0,1);
+  lcd.print("Test");
 }
 
 void loop() {
