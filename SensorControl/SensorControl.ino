ee7740078d94efe64753b8fc795190d36ec83e6f
Jeho0216 <leejen2003@naver.com>
2016-07-18 15:10:07 +0900

    밝기 측정, 출력부분을 함수화

--- SensorControl/SensorControl.ino
+++ SensorControl/SensorControl.ino
@@ -8,19 +8,27 @@ const int MIN_LIGHT = 10;   //가장 어두울 때의 값
 const int MAX_LIGHT = 720;  //가장 밝을 때의 값
 int val = A0;
 
+void printBrightness(void){
+  val = analogRead(POT);
+  val = map(val, MIN_LIGHT, MAX_LIGHT, 255, 0);   //8비트 값으로 맵핑
+  val = constrain(val, 0, 255);
+  lcd.setCursor(11,0);
+  lcd.print("   ");
+  lcd.setCursor(11,0);
+  lcd.print(val);
+  Serial.println(val);
+  delay(500);
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   lcd.begin(16, 2);
-  lcd.setCursor(0,1);
-  lcd.print("Test");
+  lcd.setCursor(0,0);
+  lcd.print("brightness:");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  val = analogRead(POT);
-  val = map(val, MIN_LIGHT, MAX_LIGHT, 255, 0);   //8비트 값으로 맵핑
-  val = constrain(val, 0, 255);
-  Serial.println(val);
-  delay(500);
+  printBrightness();
 }
