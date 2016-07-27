5c0c12a83971a943c8e9a166eebe3817dd4ec057
Lee Je-ho <Lee Je-ho>
2016-07-27 21:31:28 +0900

    스위치 디바운스 함수 수정 및 LCD 출력

--- SensorControl/SensorControl.ino
+++ SensorControl/SensorControl.ino
@@ -7,25 +7,16 @@ const int POT = 0;
 const int BTN = 14;
 const int MIN_LIGHT = 10;   //가장 어두울 때의 값
 const int MAX_LIGHT = 720;  //가장 밝을 때의 값
-
 int val = A0;
 boolean lastBTN = LOW;
 boolean currentBTN = LOW;
-
+boolean screen = 0;    //화면 번호
 
 void printBrightness(void){   //밝기값을 출력하는 함수
   val = analogRead(POT);
   val = map(val, MIN_LIGHT, MAX_LIGHT, 255, 0);   //8비트 값으로 맵핑
   val = constrain(val, 0, 255);
 }
-
-void print
-
-char valueList[3][12] ={
-   "brightness:",
-   "temp:",
-   "humidity:"
-  };
 
 boolean debounce(boolean last){
     boolean current = digitalRead(BTN);   //버튼 값 읽기
@@ -36,15 +27,36 @@ boolean debounce(boolean last){
       return current;
   }
 
+void printScreen(int page){
+    if(page == 0){
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("brightness:");
+        lcd.setCursor(0,1);
+        lcd.print("temp:");
+      }
+    else if(page == 1){
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("humidity:");
+      }
+  }
+
 void setup() {
   // put your setup code here, to run once:
-  boolean screen = 0;    //화면 번호
   Serial.begin(9600);
   pinMode(BTN, INPUT); //화면 전환 버튼
   lcd.begin(16, 2);
+  printScreen(screen);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   currentBTN = debounce(lastBTN);
+  Serial.println(currentBTN);
+  if(lastBTN == 0 && currentBTN == 1){
+      screen = !screen;
+      printScreen(screen);
+  }
+  lastBTN = currentBTN;
 }
